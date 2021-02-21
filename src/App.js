625ce75384f25e76096f4f12a753bf9378744ad8
yuriyfomin17 <fominyura2012@gmail.com>
2021-02-21 20:02:27 +0200

    Added

--- src/App.js
+++ src/App.js
@@ -2,17 +2,29 @@ import './App.css';
 import React from "react";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
 
 function App() {
     return (
         //BEM naming convention
         <div className="app">
-            <Header/>
-            <div className="app_body">
-                <Sidebar/>
-                {/*React-Router -> Chat screen */}
-            </div>
+            <Router>
+                <Header/>
+                <div className="app_body">
+                    <Sidebar/>
+                    <Switch>
+                        <Route path="/room/:roomId">
+                            <h1>CHAT SCREEN</h1>
+                            {/*Chat*/}
+                        </Route>
+                        <Route path="/">
+                            <h1>Welcome</h1>
+                        </Route>
+                    </Switch>
 
+                    {/*React-Router -> Chat screen */}
+                </div>
+            </Router>
         </div>
     );
 }
